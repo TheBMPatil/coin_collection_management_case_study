2bce8570a3e71e0816cd4afef398e46757b99665
Bhagvat Mutthe <bhagvatmutthe6@gmail.com>
2025-01-19 19:02:07 +0530

    DB Connection done

--- src/com/bm/coinmanagement/coin/Coin.java
+++ src/com/bm/coinmanagement/coin/Coin.java
@@ -1,4 +1,70 @@
 package com.bm.coinmanagement.coin;
 
-public class Coin {
+import java.time.LocalDate;
+
+public final class Coin {
+    private int id;
+    private String country;
+    private double denomination;
+    private int yearOfMint;
+    private double currentValue;
+    private LocalDate acquiredDate;
+
+
+    public Coin(double denomination, int id, String country, int yearOfMint, double currentValue, LocalDate acquiredDate) {
+        this.denomination = denomination;
+        this.id = id;
+        this.country = country;
+        this.yearOfMint = yearOfMint;
+        this.currentValue = currentValue;
+        this.acquiredDate = acquiredDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public double getDenomination() {
+        return denomination;
+    }
+
+    public void setDenomination(double denomination) {
+        this.denomination = denomination;
+    }
+
+    public int getYearOfMint() {
+        return yearOfMint;
+    }
+
+    public void setYearOfMint(int yearOfMint) {
+        this.yearOfMint = yearOfMint;
+    }
+
+    public double getCurrentValue() {
+        return currentValue;
+    }
+
+    public void setCurrentValue(double currentValue) {
+        this.currentValue = currentValue;
+    }
+
+    public LocalDate getAcquiredDate() {
+        return acquiredDate;
+    }
+
+    public void setAcquiredDate(LocalDate acquiredDate) {
+        this.acquiredDate = acquiredDate;
+    }
 }
--- src/com/bm/coinmanagement/dbconnector/DbConnector.java
+++ src/com/bm/coinmanagement/dbconnector/DbConnector.java
@@ -1,4 +1,62 @@
 package com.bm.coinmanagement.dbconnector;
 
+import com.bm.coinmanagement.coin.Coin;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DbConnector {
+
+    public static Connection connectDB() {
+        String userName = "root";
+        String password = "0787";
+        String url = "jdbc:mysql://localhost:3308/coin_management";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, userName, password);
+            System.out.println("Connection Success..!");
+            return connection;
+        } catch (SQLException e) {
+            System.err.println("Connection failed: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        String sql = "SELECT * FROM coin_collection;";
+        List<Coin> coinList = new ArrayList<>();
+
+        try (Connection connection = connectDB()) {
+            if (connection != null) {
+                Statement st = connection.createStatement();
+                ResultSet res = st.executeQuery(sql);
+
+                // Map ResultSet to Coin objects and store in ArrayList
+                while (res.next()) {
+                    int id = res.getInt("id");
+                    String country = res.getString("country");
+                    double denomination = res.getDouble("denomination");
+                    int yearOfMint = res.getInt("year_of_mint");
+                    double currentValue = res.getDouble("current_value");
+                    LocalDate acquiredDate = res.getDate("acquired_date").toLocalDate();
+
+                    // Create Coin object
+                    Coin coin = new Coin(denomination, id, country, yearOfMint, currentValue, acquiredDate);
+
+                    // Add to the list
+                    coinList.add(coin);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // Display the coins
+        System.out.println("Coins in collection:");
+        for (Coin coin : coinList) {
+            System.out.println("ID: " + coin.getId() + ", Country: " + coin.getCountry() + ", Denomination: " + coin.getDenomination() + ", Year of Mint: " + coin.getYearOfMint() + ", Current Value: " + coin.getCurrentValue() + ", Acquired Date: " + coin.getAcquiredDate());
+        }
+    }
 }
